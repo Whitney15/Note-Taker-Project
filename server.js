bf50ac52879ac2fa712854ac052360d45d3c8bcb
Whitney15 <114629880+Whitney15@users.noreply.github.com>
2023-07-06 20:14:03 -0400

    added startes code in server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+
+// setting the PORT location
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Express Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
